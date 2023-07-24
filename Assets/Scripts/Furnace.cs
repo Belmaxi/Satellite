1876a712e4da1ada3a667c13291f0d6f9d074dc4
Belmaxi <88465067+Belmaxi@users.noreply.github.com>
2023-07-24 22:27:36 +0800

    修正

---
+++ Assets/Scripts/Furnace.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Furnace : DeviceBase
+{
+    public override void Active()
+    {
+        throw new System.NotImplementedException();
+    }
+}
