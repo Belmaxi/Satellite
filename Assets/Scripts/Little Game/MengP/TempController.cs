ed64bcafd57d0858118dd2ab9de1024c36fddf96
Belmaxi <88465067+Belmaxi@users.noreply.github.com>
2023-08-24 18:01:21 +0800

    更新tmp

--- Assets/Scripts/Little Game/MengP/TempController.cs
+++ Assets/Scripts/Little Game/MengP/TempController.cs
@@ -1,18 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class TempController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private Scrollbar bar;
+    public float speed = 1f;
+    public float power = 0.1f;
+    private void Start()
     {
-        
+        bar = GetComponentInChildren<Scrollbar>();
+        bar.size = 0.7f;
     }
-
-    // Update is called once per frame
     void Update()
     {
-        
+        bar.size -= speed * Time.deltaTime;
+        Jump();
     }
+
+    void Jump()
+    {
+        if(Input.GetKeyDown(KeyCode.J))
+        {
+            bar.size += power;
+        }
+    }
+
 }
